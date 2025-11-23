68683c6d3dd89da407b33972063bc31a7c2127b8
shubhamdev <shubhamsutar5799@gmail.com>
2025-11-23 16:30:29 +0530

    web fixes

--- web/src/utils/api.ts
+++ web/src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { API_URL } from "./constants";
 
 const api = axios.create({
-  baseURL: API_URL || "http://localhost:5000/api", // fallback
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
