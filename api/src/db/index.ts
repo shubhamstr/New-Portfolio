f4439411dc95ce322ae70684462ac6f8905b36f2
shubhamdev <shubhamsutar5799@gmail.com>
2026-04-26 08:30:12 +0530

    bug fixe

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,25 +1,15 @@
-const { JsonDB, Config } = require('node-json-db')
-require('dotenv').config()
+import { JsonDB, Config } from 'node-json-db'
+import * as dotenv from 'dotenv'
 
-const dbPath = process.env.db_PATH;
-console.log(dbPath, 'dbPath')
+dotenv.config()
+
+const dbPath = process.env.db_PATH
+
+if (!dbPath) {
+  throw new Error('db_PATH is not set')
+}
 
 // true = save on push, false = pretty print
 const db = new JsonDB(new Config(dbPath, true, true, '/'))
 
-// // CREATE
-// db.push('/users[]', { id: 1, name: 'Shubham' })
-
-// // READ
-// const users = db.getData('/skills')
-// users.then((data: unknown) => {
-//   console.log(data)
-// })
-
-// // UPDATE
-// db.push('/users[0]/name', 'New Name', true)
-
-// // DELETE
-// db.delete('/users[0]')
-
-module.exports = db;
+export default db
